00141206f84ae0be628da228d65a047d7c50db82
behdad <ahmadibehdad@mail.com>
2017-10-23 16:55:46 +0330

    as'

---
+++ Hello.jsx
@@ -0,0 +1,10 @@
+const Hello = () =>{
+    return(
+        <div>
+            Hello I am Behdad with React;
+        </div>
+    );
+};
+
+ReactDOM.render(<Hello />,document.getElementsByClassName('main'));
+
